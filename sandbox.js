6258760da0ee37623dcbdf405060f84434b7563c
Patricia Carter <patricia4194@gmail.com>
2018-07-24 18:21:19 +0000

    automatically backed up by learn

---
+++ sandbox.js
@@ -0,0 +1,10 @@
+function say(greeting, firstName) {
+  return `${greeting}, ${firstName}!`
+  console.log('I was called!')
+}
+
+function sayHello() {
+  console.log('Hello!')
+}
+
+console.log('Hello')
